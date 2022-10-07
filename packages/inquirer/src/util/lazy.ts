d52f2a7a505a8f930f0b6e1e9c6eab766b361b65
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-10-07 22:48:45 +0900

    feat: lazyを実装

---
+++ packages/inquirer/src/util/lazy.ts
@@ -0,0 +1,24 @@
+export type Lazy<T, V = void> = T | ((value: V) => T);
+
+export type ResolvedLazy<T extends Lazy<unknown>, V = void> = T extends (
+  param: V
+) => unknown
+  ? ReturnType<T>
+  : T;
+
+function resolveLazy<T>(lazy: Lazy<T>, value?: void): T;
+function resolveLazy<T>(lazy: Lazy<T> | undefined, value?: void): T | undefined;
+function resolveLazy<T, V = void>(lazy: Lazy<T, V>, value: V): T;
+function resolveLazy<T, V = void>(
+  lazy: Lazy<T, V> | undefined,
+  value: V
+): T | undefined {
+  if (lazy === undefined) return undefined;
+  if (lazy instanceof Function) {
+    return lazy(value);
+  } else {
+    return lazy;
+  }
+}
+
+export { resolveLazy };
