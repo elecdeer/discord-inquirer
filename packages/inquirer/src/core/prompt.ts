553cbd45857eb4a239002d5f1784d2e4630bd719
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-10-08 23:53:04 +0900

    feat: screenを実装

--- packages/inquirer/src/core/prompt.ts
+++ packages/inquirer/src/core/prompt.ts
@@ -1,8 +1,8 @@
-import type { MessagePayload } from "../adaptor";
+import type { MessageMutualPayload } from "../adaptor/messageFacade";
 
 export type Prompt<T extends Record<string, unknown>> = (
   answer: {
     [K in keyof T]: (key: K, value: T[K]) => void;
   }[keyof T],
   close: () => void
-) => Omit<MessagePayload, "messageReference">;
+) => MessageMutualPayload;
