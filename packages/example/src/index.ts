8fd1b0a1c00e1667648728c7eabef0761c3e631c
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-10-12 04:25:16 +0900

    feat: テスト用にexampleを書いた

---
+++ packages/example/src/index.ts
@@ -0,0 +1,102 @@
+import { inquire, createScreen, useState, useEffect } from "discord-inquirer";
+import { createDiscordJsAdaptor } from "discord-inquirer-adaptor-discordjs";
+import { Client, SlashCommandBuilder } from "discord.js";
+import { config } from "dotenv";
+
+import type { Interaction } from "discord.js";
+
+config();
+
+const client = new Client({
+  intents: [],
+});
+
+client.on("ready", async (readyClient) => {
+  console.log("Client is ready");
+
+  const command = new SlashCommandBuilder()
+    .setName("example")
+    .setDescription("This is an example command");
+  await readyClient.application.commands.create(command);
+
+  readyClient.on("interactionCreate", (interaction) => {
+    if (!(interaction.isCommand() && interaction.commandName === "example")) {
+      return;
+    }
+
+    const screen = createScreen(
+      createDiscordJsAdaptor(readyClient),
+      {
+        type: "interaction",
+        interactionId: interaction.id,
+        token: interaction.token,
+      },
+      {
+        onClose: "deleteComponent",
+      }
+    );
+
+    const result = inquire(
+      (answer, close) => {
+        const [count, setCount] = useState(0);
+
+        useEffect(() => {
+          answer("count", count);
+
+          if (count >= 10) {
+            close();
+          }
+        }, [count]);
+
+        useEffect((messageId) => {
+          const handle = (interaction: Interaction) => {
+            if (!interaction.isButton()) return;
+            if (
+              interaction.message.id === messageId &&
+              interaction.customId === "customId"
+            ) {
+              interaction.deferUpdate();
+              console.log("Button clicked");
+              setCount(count + 1);
+            }
+          };
+
+          readyClient.on("interactionCreate", handle);
+
+          return () => {
+            readyClient.off("interactionCreate", handle);
+          };
+        });
+
+        return {
+          content: `count: ${count}`,
+          components: [
+            {
+              type: "row",
+              components: [
+                {
+                  type: "button",
+                  label: "increment",
+                  customId: "customId",
+                  style: "primary",
+                  url: undefined,
+                },
+              ],
+            },
+          ],
+        };
+      },
+      {
+        screen,
+      }
+    );
+
+    result.resultEvent.on(({ key, value, all }) => {
+      console.log("key", key);
+      console.log("value", value);
+      console.log("all", all);
+    });
+  });
+});
+
+await client.login(process.env.DISCORD_TOKEN);
