9f2d6fb1471d6d0075d413561a817fa18e9b750a
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-12-22 23:37:28 +0900

    feat: 新しいSelectComponentの型定義とtransformerを追加

---
+++ packages/inquirer/src/adaptor/structure/channel.ts
@@ -0,0 +1,16 @@
+/**
+ * {@link https://discord.com/developers/docs/resources/channel#channel-object-channel-types}
+ */
+export type ChannelTypes =
+  | "guildText"
+  | "dm"
+  | "guildVoice"
+  | "groupDm"
+  | "guildCategory"
+  | "guildAnnouncement"
+  | "announcementThread"
+  | "publicThread"
+  | "privateThread"
+  | "guildStageVoice"
+  | "guildDirectory"
+  | "guildForum";
