b9577daed516ff412b3ee25ec1113c633eec4bfe
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-01-18 21:17:25 +0900

    feat: useCountButtonComponentを削除して、useButtonComponentを代わりに実装

---
+++ packages/inquirer/src/hook/render/useButtonComponent.ts
@@ -0,0 +1,20 @@
+import { Button } from "../../adaptor";
+import { useButtonEvent } from "../effect/useButtonEvent";
+import { useCustomId } from "../state/useCustomId";
+
+import type { ButtonComponentBuilder } from "../../adaptor";
+
+export const useButtonComponent = (param: {
+  onClick?: () => void;
+}): ButtonComponentBuilder<{
+  customId: string;
+}> => {
+  const customId = useCustomId("button");
+
+  useButtonEvent(customId, async (_, deferUpdate) => {
+    await deferUpdate();
+    param.onClick?.();
+  });
+
+  return Button({ customId });
+};
