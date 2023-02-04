c48d19b35ee05cf6a4f148ac04002fd3ee339b85
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-02-04 21:49:33 +0900

    feat: timerの実装を置き換え

--- packages/inquirer/src/util/timer.ts
+++ packages/inquirer/src/util/timer.ts
@@ -1,65 +1,4 @@
-type Awaitable<T> = T | PromiseLike<T>;
-
-export interface Timer {
-  /**
-   * handlerを渡しタイマーを開始する
-   * @param handler
-   */
-  start: (handler: () => Awaitable<void>) => void;
-
-  /**
-   * まだhandlerが呼ばれていない場合、タイマーをリセットする
-   */
-  reset: () => void;
-
-  /**
-   * タイマーを停止する
-   */
-  dispose: () => void;
-
-  /**
-   * タイマーが停止あるいは終了したかどうか
-   */
-  finished: () => boolean;
-}
-
-export const createTimer = (timeMs: number): Timer => {
-  let status: {
-    timer: NodeJS.Timeout;
-    handler: () => Awaitable<void>;
-  } | null;
-
-  const start = (handler: () => Awaitable<void>) => {
-    const timer = setTimeout(() => {
-      void handler();
-      status = null;
-    }, timeMs);
-
-    status = {
-      timer: timer,
-      handler: handler,
-    };
-  };
-
-  return {
-    start: start,
-    reset: () => {
-      if (status !== null) {
-        clearTimeout(status.timer);
-        start(status.handler);
-      }
-    },
-    dispose: () => {
-      if (status !== null) {
-        clearTimeout(status.timer);
-        status = null;
-      }
-    },
-    finished: () => status === null,
-  };
-};
-
-export const createTimer2 = (timeoutMs: number) => {
+export const createTimer = (timeoutMs: number) => {
   let timeoutId: NodeJS.Timeout | null = null;
   let timeout = false;
   const handlers: (() => void)[] = [];
@@ -88,6 +27,7 @@ export const createTimer2 = (timeoutMs: number) => {
 
   const addHandler = (handler: () => void) => {
     handlers.push(handler);
+    return timer;
   };
 
   const wait = async () => {
@@ -99,11 +39,13 @@ export const createTimer2 = (timeoutMs: number) => {
 
   start();
 
-  return {
+  const timer = {
     reset,
     dispose,
     isTimeout,
     onTimeout: addHandler,
     wait,
   };
+
+  return timer;
 };
