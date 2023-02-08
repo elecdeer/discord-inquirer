c5eb25e9f733a0217a7828c958d4c1f733e3fbfc
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-02-08 15:20:48 +0900

    feat: useModalComponentのテストを追加

---
+++ packages/inquirer/src/hook/render/useModalComponent.test.ts
@@ -0,0 +1,58 @@
+import { describe, expect, test } from "vitest";
+
+import { useModalComponent } from "./useModalComponent";
+import { renderHook } from "../../testing";
+
+import type { AdaptorInteractionResponse } from "../../adaptor";
+
+describe("packages/inquirer/src/hook/render/useModalComponent", () => {
+  describe("useModalComponent()", () => {
+    test("ボタンをクリックするとmodalが開く", () => {
+      const { result, interactionHelper, waitFor, adaptorMock } = renderHook(
+        () =>
+          useModalComponent({
+            title: "title",
+            components: [
+              {
+                style: "short",
+                key: "foo",
+                label: "foo",
+              },
+            ],
+          })
+      );
+
+      const component = result.current[1]({
+        style: "primary",
+      })();
+      const interaction = interactionHelper.clickButtonComponent(component);
+
+      waitFor(() =>
+        expect(adaptorMock.sendInteractionResponse).toBeCalledWith(
+          interaction.id,
+          interaction.token,
+          {
+            type: "modal",
+            data: {
+              title: "title",
+              customId: expect.any(String),
+              components: [
+                {
+                  type: "row",
+                  components: [
+                    {
+                      type: "textInput",
+                      customId: expect.any(String),
+                      style: "short",
+                      label: "foo",
+                    },
+                  ],
+                },
+              ],
+            },
+          } satisfies AdaptorInteractionResponse
+        )
+      );
+    });
+  });
+});
--- packages/inquirer/src/hook/render/useModalComponent.ts
+++ packages/inquirer/src/hook/render/useModalComponent.ts
@@ -3,21 +3,34 @@ import { useButtonEvent } from "../effect/useButtonEvent";
 import { useModal } from "../effect/useModal";
 import { useCustomId } from "../state/useCustomId";
 
-import type { ButtonComponentBuilder } from "../../adaptor";
+import type { NonLinkButtonComponentBuilder } from "../../adaptor";
 import type { UseModalParam } from "../effect/useModal";
 
 export type UseModalComponentResult<TKey extends string> = [
   result: Record<TKey, string> | null,
-  Button: ButtonComponentBuilder<{
+  Button: NonLinkButtonComponentBuilder<{
     customId: string;
   }>
 ];
 
-export const useModalComponent = <TKey extends string>(
-  param: UseModalParam<TKey>
-): UseModalComponentResult<TKey> => {
+/**
+ * モーダルを開くボタンとモーダルの結果を返すrenderHook
+ * @param onSubmit モーダルの結果が返ってきたときに実行されるハンドラ
+ * @param title モーダルのタイトル
+ * @param components モーダルに表示するコンポーネント
+ * @returns [result, NonLinkButtonComponentBuilder]
+ */
+export const useModalComponent = <TKey extends string>({
+  onSubmit,
+  title,
+  components,
+}: UseModalParam<TKey>): UseModalComponentResult<TKey> => {
   const customId = useCustomId("modalButton");
-  const [result, openModal] = useModal(param);
+  const [result, openModal] = useModal({
+    onSubmit,
+    components,
+    title,
+  });
 
   useButtonEvent(customId, async (interaction) => {
     openModal(interaction.id, interaction.token);
