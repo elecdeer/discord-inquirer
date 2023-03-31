3afda147930a64ad7d674aa5cba1077ef5cf099d
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-04-01 01:52:21 +0900

    chore: @noble/ed25519のアップデートとそれに伴う修正

--- packages/node-adaptor/src/server.ts
+++ packages/node-adaptor/src/server.ts
@@ -1,6 +1,8 @@
 import { createEventFlow } from "@elecdeer/event-flow";
-import { verify } from "@noble/ed25519";
+import { verifyAsync, etc } from "@noble/ed25519";
+import { sha512 } from "@noble/hashes/sha512";
 import * as http from "http";
+import { webcrypto } from "node:crypto";
 
 import type {
   APIInteraction,
@@ -83,13 +85,23 @@ const parseBody = (req: http.IncomingMessage) =>
     });
   });
 
+//https://github.com/paulmillr/noble-ed25519#usage
+
+//@noble/ed25519のverifyAsyncは@noble/hashes/sha512を使うので、依存性を注入する必要がある
+etc.sha512Sync = (...m) => sha512(etc.concatBytes(...m));
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+if (!globalThis.crypto) globalThis.crypto = webcrypto;
+
 const verifyKey = async (
   body: Buffer,
   signature: string,
   timestamp: string,
   publicKey: string
 ) => {
-  return await verify(
+  return await verifyAsync(
     signature,
     Buffer.from(timestamp + body.toString()),
     publicKey
