9daf3628beacbc18f07febc4d67707db9ca5ac2d
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-04-01 01:54:49 +0900

    refactor: 検証部分を切り出し

--- packages/node-adaptor/src/server.ts
+++ packages/node-adaptor/src/server.ts
@@ -1,8 +1,7 @@
 import { createEventFlow } from "@elecdeer/event-flow";
-import { verifyAsync, etc } from "@noble/ed25519";
-import { sha512 } from "@noble/hashes/sha512";
 import * as http from "http";
-import { webcrypto } from "node:crypto";
+
+import { verifyKey } from "./verifyKey";
 
 import type {
   APIInteraction,
@@ -84,26 +83,3 @@ const parseBody = (req: http.IncomingMessage) =>
       resolve(Buffer.concat(chunks));
     });
   });
-
-//https://github.com/paulmillr/noble-ed25519#usage
-
-//@noble/ed25519のverifyAsyncは@noble/hashes/sha512を使うので、依存性を注入する必要がある
-etc.sha512Sync = (...m) => sha512(etc.concatBytes(...m));
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-if (!globalThis.crypto) globalThis.crypto = webcrypto;
-
-const verifyKey = async (
-  body: Buffer,
-  signature: string,
-  timestamp: string,
-  publicKey: string
-) => {
-  return await verifyAsync(
-    signature,
-    Buffer.from(timestamp + body.toString()),
-    publicKey
-  );
-};
