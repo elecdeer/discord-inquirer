e20e25283056f59ed4811d0bdf09d4d3ce4bed03
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-08-05 17:06:45 +0900

    feat: flat configでeslint設定を再構成

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,68 +1,93 @@
-module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-    project: "./tsconfig.base.json",
-  },
-  plugins: ["@typescript-eslint", "import"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  rules: {
-    // recommended overrides
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-        destructuredArrayIgnorePattern: "^_",
-      },
-    ],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    // custom rules
-    "@typescript-eslint/await-thenable": "error",
-    "@typescript-eslint/consistent-type-imports": "error",
-    "@typescript-eslint/strict-boolean-expressions": [
-      "error",
-      {
-        allowString: false,
-        allowNumber: false,
-        allowNullableBoolean: true,
-      },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: ["*/src"],
-      },
-    ],
-    "import/order": [
-      "error",
-      {
-        groups: [
-          ["builtin", "external"],
-          ["parent", "sibling", "index"],
-          "object",
-          "type",
-        ],
-        pathGroups: [
-          {
-            pattern: "@alias/**",
-            group: "parent",
-            position: "before",
-          },
-        ],
-        alphabetize: {
-          order: "asc",
+const prettier = require("eslint-config-prettier");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+const importPlugin = require("eslint-plugin-import");
+
+const eslintConfig = [
+  {
+    files: ["**/*.js", "*.js"],
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      ...prettier.rules,
+      ...importPlugin.configs.recommended.rules,
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: ["*/src"],
         },
-        "newlines-between": "always",
-      },
-    ],
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "@alias/**",
+              group: "parent",
+              position: "before",
+            },
+          ],
+          alphabetize: {
+            order: "asc",
+          },
+          "newlines-between": "always",
+        },
+      ],
+    },
   },
-};
+  {
+    files: ["**/*.ts", "*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: "module",
+        project: "tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      import: importPlugin,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "tsconfig.json",
+        },
+      },
+    },
+    rules: {
+      ...tsPlugin.configs["recommended-type-checked"].rules,
+      ...tsPlugin.configs["stylistic-type-checked"].rules,
+      // recommended overrides
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/require-await": "off",
+      // custom rules
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/strict-boolean-expressions": [
+        "error",
+        {
+          allowString: false,
+          allowNumber: false,
+          allowNullableBoolean: true,
+        },
+      ],
+    },
+  },
+];
+
+module.exports = eslintConfig;
