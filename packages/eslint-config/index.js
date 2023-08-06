b1817df7efe68cd1cd6fdf32ad46faefd49131f0
elecdeer <37701077+elecdeer@users.noreply.github.com>
2023-08-06 22:26:04 +0900

    feat: .test.tsではany系ruleを無効に

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,90 +3,108 @@ const tsParser = require("@typescript-eslint/parser");
 const prettier = require("eslint-config-prettier");
 const importPlugin = require("eslint-plugin-import");
 
+const configForJs = {
+  plugins: {
+    import: importPlugin,
+  },
+  rules: {
+    ...prettier.rules,
+    ...importPlugin.configs.recommended.rules,
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: ["*/src"],
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          ["parent", "sibling", "index"],
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "@alias/**",
+            group: "parent",
+            position: "before",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
+
+const configForTs = {
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      sourceType: "module",
+      project: true, // コードから最も近いtsconfig.json
+    },
+  },
+  plugins: {
+    "@typescript-eslint": tsPlugin,
+    import: importPlugin,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project: "tsconfig.json",
+      },
+    },
+  },
+  rules: {
+    ...tsPlugin.configs["recommended-type-checked"].rules,
+    ...tsPlugin.configs["stylistic-type-checked"].rules,
+    // recommended overrides
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/require-await": "off",
+    // custom rules
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/strict-boolean-expressions": [
+      "error",
+      {
+        allowString: false,
+        allowNumber: false,
+        allowNullableBoolean: true,
+      },
+    ],
+  },
+};
+
 const eslintConfig = [
   {
     files: ["**/*.js", "*.js"],
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      ...prettier.rules,
-      ...importPlugin.configs.recommended.rules,
-      "no-restricted-imports": [
-        "error",
-        {
-          patterns: ["*/src"],
-        },
-      ],
-      "import/order": [
-        "error",
-        {
-          groups: [
-            ["builtin", "external"],
-            ["parent", "sibling", "index"],
-            "object",
-            "type",
-          ],
-          pathGroups: [
-            {
-              pattern: "@alias/**",
-              group: "parent",
-              position: "before",
-            },
-          ],
-          alphabetize: {
-            order: "asc",
-          },
-          "newlines-between": "always",
-        },
-      ],
-    },
+    ...configForJs,
   },
   {
     files: ["**/*.ts", "*.ts"],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        sourceType: "module",
-        project: true, // コードから最も近いtsconfig.json
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      import: importPlugin,
-    },
-    settings: {
-      "import/resolver": {
-        typescript: {
-          project: "tsconfig.json",
-        },
-      },
-    },
+    ignores: ["**/*.test.ts", "*.test.ts"],
+    ...configForTs,
+  },
+  {
+    files: ["**/*.test.ts", "*.test.ts"],
+    ...configForTs,
     rules: {
-      ...tsPlugin.configs["recommended-type-checked"].rules,
-      ...tsPlugin.configs["stylistic-type-checked"].rules,
-      // recommended overrides
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
-          destructuredArrayIgnorePattern: "^_",
-        },
-      ],
-      "@typescript-eslint/require-await": "off",
-      // custom rules
-      "@typescript-eslint/consistent-type-imports": "error",
-      "@typescript-eslint/strict-boolean-expressions": [
-        "error",
-        {
-          allowString: false,
-          allowNumber: false,
-          allowNullableBoolean: true,
-        },
-      ],
+      ...configForTs.rules,
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
     },
   },
 ];
